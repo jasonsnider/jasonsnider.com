8956ec673076b47e13e60be7ced417417cee7055
Jason Snider <root@jasonsnider.com>
2016-09-15 13:06:08 -0500

    The default layout noe detects meta data for blog posts

--- plugin/Content/src/View/layouts/default.php
+++ plugin/Content/src/View/layouts/default.php
@@ -5,11 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
+
+    <meta name="description" content="<?php !empty($View->vars['post']['description'])?$View->vars['post']['description']:''; ?>">
+    <meta name="keywords" content="<?php !empty($View->vars['post']['keywords'])?$View->vars['post']['keywords']:''; ?>">
     <meta name="author" content="Jason Snider">
     <!--<link rel="icon" href="favicon.ico">-->
 
-    <title>Jason Snider</title>
+    <title><?php echo !empty($View->vars['post']['title'])?$View->vars['post']['title']:'Jason Snider [dot] com'; ?></title>
 
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
