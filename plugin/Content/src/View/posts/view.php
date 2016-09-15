232f7c602e09b8cb79399455ef666ce94ca31875
Jason Snider <root@jasonsnider.com>
2016-09-15 12:46:40 -0500

    Added an about the author section

--- plugin/Content/src/View/posts/view.php
+++ plugin/Content/src/View/posts/view.php
@@ -20,3 +20,22 @@ if(!empty($next['slug'])){
 }
 
 echo "</ul></nav>";
+
+?>
+<hr>
+<aside itemscope itemtype="http://data-vocabulary.org/Person">
+    <div class="media">
+        <div class="media-left">
+            <a href="#">
+                <img alt="64x64" class="media-object thumbnail" src="https://secure.gravatar.com/avatar/57dd069b73a149098c4865f8f5813303.png" height="80px" width="80px" style="margin-bottom: 0;">
+            </a>
+        </div>
+        <div class="media-body">
+            <h4 class="media-heading">About the Author</h4>
+            Hi, I'm
+            <a href="https://plus.google.com/+JasonSnider?rel=author" target="_blank" itemprop="name">Jason Snider</a>
+            a <span itemprop="title">full stack web developer, designer, systems architect</span>, security enthusiast,
+            Linux aficionado, open source advocate, sys admin, DBA, business analyst, project manager and scrum master.
+        </div>
+    </div>
+</aside>
